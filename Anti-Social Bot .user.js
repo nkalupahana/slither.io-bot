f839dfd3e4f1c706d2c36676641dc0f8676027eb
Toll_Micah <tollm248@hsd.k12.or.us>
2019-05-28 11:39:26 -0700

    added snake avoidance (does not see tail)

--- Anti-Social Bot .user.js
+++ Anti-Social Bot .user.js
@@ -28,4 +28,28 @@
             }, 250);
         }
     };
+
+    /*document.onkeydown = function (e) {
+        e = e || window.event;
+        console.log(e);
+        if (e.keyCode == 87) {
+            let minCombinedDistance = Infinity;
+            let snakeId = unsafeWindow.snake["id"];
+            let closestFoe = null;
+            for (let snake of unsafeWindow.snakes) {
+                if (snakeId != snake["id"]) {
+                    if (snake != null) {
+                        let dist = Math.pow(Math.pow(snake.xx - unsafeWindow.snake.xx, 2) + Math.pow(snake.yy - unsafeWindow.snake.yy, 2), 0.5);
+                        if (minCombinedDistance > dist) {
+                            minCombinedDistance = dist;
+                            closestFoe = snake;
+                        }
+                    }
+                }
+            }
+
+            unsafeWindow.xm = -(closestFoe.xx - Math.round(unsafeWindow.snake.xx));
+            unsafeWindow.ym = -(closestFoe.yy - Math.round(unsafeWindow.snake.yy));
+        }
+    };*/
 })();
